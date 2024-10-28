ecc4d8034b0eea147da59207c176b1a84cf86476
Nttt03 <thanhthao03bd@gmail.com>
2024-10-28 13:12:05 +0700

    52_Import Data from EXCEL and write API GET ALLCODE

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -64,11 +64,25 @@ let handleDeleteUser = async (req, res) => {
     return res.status(200).json(message);
 }
 
+let getAllCode = async (req, res) => {
+    try {
+        let data = await userServise.getAllCodeService(req.query.type);
+        return res.status(200).json(data);
+    } catch (e) {
+        console.log('Get all code error: ', e);
+        return res.status(200).json({
+            errCode: -1,
+            errMessage: "Error from server"
+        })
+    }
+}
+
 module.exports = {
     handleLogin: handleLogin,
     handleGetAllUsers: handleGetAllUsers,
     handleCreateNewUser: handleCreateNewUser,
     handleEditUser: handleEditUser,
     handleDeleteUser: handleDeleteUser,
+    getAllCode: getAllCode,
 
 }
--- src/route/web.js
+++ src/route/web.js
@@ -18,6 +18,8 @@ let initWebRoutes = (app) => {
     router.post('/api/create-new-user', userController.handleCreateNewUser);
     router.put('/api/edit-user', userController.handleEditUser);
     router.delete('/api/delete-user', userController.handleDeleteUser);
+
+    router.get("/allcode", userController.getAllCode);
     
     // router.get('/hoidanit', (rep, res) => {
     //     return res.send("Hế lô hoidanit ^_^ !")
