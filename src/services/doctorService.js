b57c903dff4e0e7f9199e24161486db334eca7a5
Nttt03 <thanhthao03bd@gmail.com>
2025-02-22 11:44:17 +0700

    69_Fix func get detail doctor_Creat Page Interface to View Detailed Doctor Information...

--- src/services/doctorService.js
+++ src/services/doctorService.js
@@ -88,7 +88,7 @@ let getDetailDoctorByIdService = (inputId) => {
                         id: inputId
                     },
                     attributes: {
-                        exclude: ['password', 'image']
+                        exclude: ['password']
                     },
                     include: [ 
                         { 
@@ -97,9 +97,15 @@ let getDetailDoctorByIdService = (inputId) => {
                         },
                         { model: db.Allcode, as: 'positionData', attributes: ['valueEn', 'valueVi'] },
                     ],
-                    raw: true,
+                    raw: false,
                     nest: true
                 })
+                
+                if(data && data.image) {
+                    data.image = new Buffer(data.image, 'base64').toString('binary');
+                }
+                if(!data) { data = {} }
+
                 resolve({
                     errCode: 0,
                     data: data
