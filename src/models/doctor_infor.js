2bf02d40787bc882dd8e740634bf30d5d1fd5167
nttt03 <thanhthao03bd@gmail.com>
2025-03-18 11:39:48 +0700

    78_Create more Doctor_Infor table...

---
+++ src/models/doctor_infor.js
@@ -0,0 +1,26 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class Doctor_Infor extends Model {
+    static associate(models) {
+      // define association here
+    }
+  };
+  
+  Doctor_Infor.init({
+    doctorId: DataTypes.INTEGER,
+    priceId: DataTypes.STRING,
+    provinceId: DataTypes.STRING,
+    paymentId: DataTypes.STRING,
+    addressClinic: DataTypes.STRING,
+    nameClinic: DataTypes.STRING,
+    note: DataTypes.STRING,
+    count: DataTypes.INTEGER,
+  }, {
+    sequelize,
+    modelName: 'Doctor_Infor',
+  });
+  return Doctor_Infor;
+};
